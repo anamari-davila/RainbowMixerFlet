b3e55a8cde68203e7fbe2bd9054b50fd18c3a201
anamari-davila <anamaridaviladejesus@gmail.com>
2025-04-08 08:33:17 -0400

    Added the Whole assingment

--- Rainbow.py
+++ Rainbow.py
@@ -1,7 +1,29 @@
 import flet as ft 
 
 def main(page: ft.Page):
-    #Ill work on this later
-    pass
+    def BackgroundColor(e):
+        r = red.value
+        redvalue.value = r
+        g = green.value
+        greenvalue.value = g
+        b = blue.value
+        bluevalue.value = b
+        background = f"#{int(r):02x}{int(g):02x}{int(b):02x}"
+        HexValue.value = f"#{int(r):02x}{int(g):02x}{int(b):02x}"
+        page.bgcolor = background
+        page.update()
+
+
+    Title = ft.Text(value="Background Color Picker", size=20)
+    red = ft.Slider(value=0, min=0, max=255, divisions=255, label="{value}%", on_change=BackgroundColor)
+    redvalue = ft.Text(value="")
+    blue = ft.Slider(value=0, min=0, max=255, divisions=255, label="{value}%", on_change=BackgroundColor)
+    bluevalue = ft.Text(value="")
+    green = ft.Slider(value=0, min=0, max=255, divisions=255, label="{value}%", on_change=BackgroundColor)
+    greenvalue = ft.Text(value="")
+    HexValue = ft.Text(value="#000000")
+
+    ColumnMain = ft.Column(controls=[red, redvalue, blue,bluevalue, green, greenvalue, HexValue])
+    page.add(Title, ColumnMain) 
 
 ft.app(target=main)
